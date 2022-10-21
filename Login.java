6150bba3f87f65c89e0e0ddcbbc98d4dc2f782e0
manantpu <112652839+manantpu@users.noreply.github.com>
2022-10-20 18:55:32 -0700

    Login functionality with file reading

--- Login.java
+++ Login.java
@@ -21,7 +21,53 @@ public class Login {
 		Login() {
 			userType = 0;
 		}
-		
+		public int login() {
+            System.out.println("Login Initiated ");
+            System.out.println("Enter Username ");
+            @SuppressWarnings("resource")
+            Scanner scan = new Scanner(System.in);
+            String username = scan.next();
+            System.out.println("Enter Password ");
+            String password = scan.next();
+            userType = validation(username, password);
+            return userType;
+        }
+
+        public static int validation(String username, String password) {
+            int userType=-1;
+            String[][] files = {{"data/BuyerInfo.txt", "Buyer"},{"data/SellerInfo.txt", "Seller"}};
+
+            for(int j=0; j<2; j++){
+                Path filePath = Paths.get(files[j][0]);
+                if(files[j][1] == "Buyer"){
+                    userType = 0;
+                } else {
+                    userType = 1;
+                }
+                String fileContent = null;
+                try {
+                    fileContent = Files.readString(filePath);
+                } catch (IOException e) {
+                    System.out.println("WARNING: DATABASE READ ERROR");
+                }
+                String[] lines = fileContent.split("\\r?\\n");
+                for(int i=0; i< lines.length; i++){
+                        if(lines[i].split(":")[0].equals(username)){
+                            if(lines[i].split(":")[1].equals(password)){
+                                if(userType == 0) {
+                                    System.out.println("Welcome "+username);
+                                    return userType;
+                                } else {
+                                    System.out.println("Welcome "+username);
+                                    return userType;
+                                }
+                            }
+                        }
+                    }
+                }
+            return userType;
+        }
+
 }
 		
 
